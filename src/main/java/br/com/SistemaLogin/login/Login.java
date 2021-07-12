b711bcedb10236724921a49189ad4824b6ae2e85
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-12 00:53:36 -0300

    class login

---
+++ src/main/java/br/com/SistemaLogin/login/Login.java
@@ -0,0 +1,36 @@
+package br.com.SistemaLogin.login;
+
+public class Login {
+
+	private int id_usuario;
+	private String nome_usuario;
+	private String senha_usuario;
+
+	
+	
+	// metodos de envio e recebimento de dados
+	public int getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNome_usuario() {
+		return nome_usuario;
+	}
+
+	public void setNome_usuario(String nome_usuario) {
+		this.nome_usuario = nome_usuario;
+	}
+
+	public String getSenha_usuario() {
+		return senha_usuario;
+	}
+
+	public void setSenha_usuario(String senha_usuario) {
+		this.senha_usuario = senha_usuario;
+	}
+
+}

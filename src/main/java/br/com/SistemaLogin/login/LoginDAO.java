334dbe77888e22560d2f63e62c7219c223d40a06
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-12 01:04:14 -0300

    Sobre dao

--- src/main/java/br/com/SistemaLogin/login/LoginDAO.java
+++ src/main/java/br/com/SistemaLogin/login/LoginDAO.java
@@ -1,3 +1,11 @@
+/*Classes DAO são responsáveis por trocar informações com o SGBD e fornecer operações CRUD e de pesquisas, 
+ * elas devem ser capazes de buscar dados no banco e transformar esses em objetos ou lista de objetos,
+ *  fazendo uso de listas genéricas (BOX 3), também deverão receber os objetos, 
+ *  converter em instruções SQL e mandar para o banco de dados.
+ *  
+ *  Fonte: https://www.devmedia.com.br/dao-pattern-persistencia-de-dados-utilizando-o-padrao-dao/30999#:~:text=Classes%20DAO%20são%20responsáveis%20por,em%20instruções%20SQL%20e%20mandar
+ *  */
+
 package br.com.SistemaLogin.login;
 
 public class LoginDAO {
